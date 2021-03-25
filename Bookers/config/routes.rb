5048d57332d939f903a84cf1b94f3647cb571085
haaanyan <kiha.0327@icloud.com>
2021-03-25 11:13:58 +0000

    [Fix3]3-8

--- Bookers/config/routes.rb
+++ Bookers/config/routes.rb
@@ -4,4 +4,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   post 'books' => 'books#create'
   get 'books' => 'books#index'
+  get 'books/:id' => 'books#show'
+  get 'books/:id/edit' => 'books#edit'
+  patch 'books/:id' => 'books#update'
+  delete 'books/:id' => 'books#destroy'
 end
